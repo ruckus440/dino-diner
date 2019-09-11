a78f36abd77df95528e1f13986807e7e07b1f5cc
Mike Ruckert <mwr3333@ksu.edu>
2019-09-11 15:04:03 -0500

    Restart

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private bool chicken = false;
+        private bool wingSauce = false;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59 * 6;
+        }
+
+        public void AddNugget()
+        {
+            this.Price += .25;
+            this.Calories += 59;
+        }
+
+    }
+}
