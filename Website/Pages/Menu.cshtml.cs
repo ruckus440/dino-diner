c557bba68171c6b6fecbdd4bda9a0b676ea0e3cb
Mike Ruckert <mwruckert@gmail.com>
2019-11-17 22:06:48 -0600

    Website Milestone 1 done

---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,34 @@
+﻿/* Menu.cshtml.cs
+ * Author: Mike Ruckert
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DinoDiner.Menu;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+        /// <summary>
+        /// Private backing variable
+        /// </summary>
+        private Menu menu;
+
+        /// <summary>
+        /// Read only Menu property
+        /// </summary>
+        public Menu Menu { get { return menu; } }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public void OnGet()
+        {
+            menu = new Menu();
+        }
+    }
+}
